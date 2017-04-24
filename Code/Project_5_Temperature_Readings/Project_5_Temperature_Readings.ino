2d07abccdfd136fc58fbe904a7b87d42d28260bd
Rui Santos <ee11077@fe.up.pt>
2017-04-24 11:10:15 +0100

    Removed unnecessary comments

--- Code/Project_5_Temperature_Readings/Project_5_Temperature_Readings.ino
+++ Code/Project_5_Temperature_Readings/Project_5_Temperature_Readings.ino
@@ -35,11 +35,11 @@ void loop() {
     // Call sensors.requestTemperatures() to issue a global temperature and Requests to all devices on the bus
     sensors.requestTemperatures(); 
     
-    //Serial.print("Celsius temperature: ");
+    // Celsius temperature
     // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
     Serial.print(sensors.getTempCByIndex(0));
  
-    //Serial.print(" - Fahrenheit temperature: ");
+    // Fahrenheit temperature
     //Serial.println(sensors.getTempFByIndex(0));
   }
 }
